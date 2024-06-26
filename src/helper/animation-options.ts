f241f55a71cc02cf13f02e3e32b4411349de5864
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2024-06-26 17:03:06 +0100

    refactor: Added transition animation to the login page

--- src/helper/animation-options.ts
+++ src/helper/animation-options.ts
@@ -2,3 +2,18 @@ export const dropdownAnimation = {
     hidden: { opacity: 0, scale: 0.95 },
     visible: { opacity: 1, scale: 1 }
 };
+
+
+export const loginContainerVariants = {
+    hidden: { opacity: 0, x: -200 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: { type: 'spring', stiffness: 120 }
+    },
+    exit: {
+        x: -200,
+        opacity: 0,
+        transition: { ease: 'easeInOut' }
+    }
+};
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,10 +1,12 @@
 import { Button, Form, Input } from "antd";
-import { SignInImage } from "../../assets";
+import { LogoIcon, SignInImage } from "../../assets";
 import "../styles/auth.scss";
 import { useAuth } from "../../hooks/use-auth";
 import { saveLocalStorage } from "../../helper/storage";
 import { useNavigate } from "@tanstack/react-router"
 import { useState } from "react";
+import { motion } from "framer-motion";
+import { loginContainerVariants } from "../../helper/animation-options";
 export default function Login() {
 
     const { setIsUserLoggedIn, setUserData } = useAuth();
@@ -33,21 +35,35 @@ export default function Login() {
 
     }
     return (
-        <div className="auth-layout">
+        <motion.div
+            variants={loginContainerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="auth-layout login-font">
             <div className="auth-image">
                 <img src={SignInImage} alt="sign in img" />
             </div>
 
+
             <div className="auth-form">
-                <p>Login into your Account</p>
+
+                <div className="auth-text logo-icon-auth">
+                    <img src={LogoIcon} alt="logo" />
+                </div>
+
+                <div className="auth-text login-font">
+                    <h2>Welcome!</h2>
+                    <p>Enter details to Login</p>
+                </div>
                 <Form className="auth-form-form" onFinish={onSubmit}>
-                    
+
                     <Form.Item label="" name="email" rules={[{ required: true }]}>
                         <Input placeholder="Email" className="input-email" />
                     </Form.Item>
 
                     <Form.Item name="password" label="" rules={[{ required: true }]}>
-                        <Input placeholder="password" type="password" className="input-password" />
+                        <Input placeholder="Password" type="password" className="input-password" />
                     </Form.Item>
 
                     <p className="forgot-pwd-text">
@@ -57,7 +73,7 @@ export default function Login() {
                     <Button loading={isLoadingAuth} className="auth-btn" htmlType="submit" type="primary">Login</Button>
                 </Form>
             </div>
-        </div>
+        </motion.div>
 
     )
 }
