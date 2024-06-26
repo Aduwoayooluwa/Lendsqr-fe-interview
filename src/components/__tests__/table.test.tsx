f17bfa9fbf1de1e82cc4fe89750f564fba3a9def
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2024-06-26 03:43:22 +0100

    fix: tests in table.test.tsx

--- src/components/__tests__/table.test.tsx
+++ src/components/__tests__/table.test.tsx
@@ -1,4 +1,9 @@
-import { describe, it, beforeEach, vi } from 'vitest';
+// import { Table } from 'antd';
+import { describe, it, beforeEach, vi, expect,  } from 'vitest';
+import { fetchData } from '../../config/api';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 // import { fetchData } from '../../config/api';
 
 
@@ -13,58 +18,59 @@ describe('Table ', () => {
 
 
   it('should return JSON data on successful fetch', async () => {
-    // const mockJson = { key: 'value' };
-    // (window.fetch as vi.Mock).mockResolvedValueOnce({
-      // ok: true,
-      // json: () => Promise.resolve(mockJson),
-      // status: 200,
-    // });
+    const mockJson = { key: 'value' };
+    (window.fetch as unknown as vi.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve(mockJson),
+      status: 200,
+    });
 
-    // const url = '/data/all-users.json';
-    // const result = await fetchData<typeof mockJson>(url, new AbortController().signal);
+    const url = '/data/all-users.json';
+    const result = await fetchData<typeof mockJson>(url, new AbortController().signal);
 
-    // expect(result).toEqual(mockJson);
-    // expect(fetch).toHaveBeenCalledWith(url, { signal: expect.any(AbortSignal) });
+    expect(result).toEqual(mockJson);
+    expect(fetch).toHaveBeenCalledWith(url, { signal: expect.any(AbortSignal) });
   });
 
   it('should throw an error on non-200 status', async () => {
-    // (window.fetch as vi.Mock).mockResolvedValueOnce({
-      // ok: false,
-      // status: 404,
-    // });
+    (window.fetch as vi.Mock).mockResolvedValueOnce({
+      ok: false,
+      status: 404,
+    });
 
-    // const url = '/data/bad-request';
+    const url = '/data/bad-request';
 
-    // await expect(fetchData<{}>(url, new AbortController().signal))
-      // .rejects
-      //.toThrow('HTTP error! Status: 404');
+    await expect(fetchData<{}>(url, new AbortController().signal))
+      .rejects
+      .toThrow('HTTP error! Status: 404');
   });
 
   it('should handle network or fetch errors', async () => {
-    // (window.fetch as vi.Mock).mockRejectedValueOnce(new Error('Network failure'));
+    //  eslint-disable-next-line
+    (window.fetch as vi.Mock).mockRejectedValueOnce(new Error('Network failure'));
 
-    // const url = '/data/fail';
+    const url = '/data/fail';
 
-    // await expect(fetchData<{}>(url, new AbortController().signal))
-    //   .rejects
-    //   .toThrow('Network failure');
+    await expect(fetchData<{}>(url, new AbortController().signal))
+      .rejects
+      .toThrow('Network failure');
   });
 
     it('renders the table and displays data after loading',  async () => {
-   // const { getAllByTestId } = render(<Table />);
-    
+  // const { getAllByTestId } = render(<Table />);
+    // 
 
-   // Check for the data presence
-    // expect(screen.getByText('Tech Co')).toBeInTheDocument();
-    // expect(screen.getByText('jdoe@example.com')).toBeInTheDocument();
+  // Check for the data presence
+    expect(screen.getByText('Tech Co')).toBeInTheDocument();
+    expect(screen.getByText('jdoe@example.com')).toBeInTheDocument();
 
-   // check for custom components like tags
-    // const statusTag = screen.getByText('ACTIVE');
-    // expect(statusTag).toBeInTheDocument();
-    // expect(statusTag).toHaveClass('ant-tag-green'); 
+   //check for custom components like tags
+    const statusTag = screen.getByText('ACTIVE');
+    expect(statusTag).toBeInTheDocument();
+    expect(statusTag).toHaveClass('ant-tag-green'); 
 
-   // check for actions or links
-   // expect(screen.getByText('View Details').closest('a')).toHaveAttribute('href', '/users-details');
+  // check for actions or links
+   expect(screen.getByText('View Details').closest('a')).toHaveAttribute('href', '/users-details');
   });
 
   it("Check if the filter form is opening", () => {
